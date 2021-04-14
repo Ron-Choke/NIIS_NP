1b87a83e1b8fa0de756fac730091b868bea6ca55
Ronpichai C <Ronpichai.c@chula.ac.th>
2021-04-14 16:33:25 +0700

    test to commit with ICESWAN

--- NIIS.R
+++ NIIS.R
@@ -1,2 +1,14 @@
 library(haven)
+library(tidyverse)
 dat <- read_sav("/Users/ronpichaic/Dropbox/Yang_dropbox/Research/AIEM/NIIS_NP/merge0304.SAV")
+re_dat <- dat[,c(1,3,5,11,19,35,37,53,68,86,87)]
+ggplot(data = re_dat)+
+  geom_histogram(mapping = aes(x= LOS))
+
+ggplot(data = re_dat, mapping =aes(x= LOS, colour = as.factor(FEMALE)))+
+  geom_freqpoly(binwidth=0.1)+
+  coord_cartesian(xlim = c(0,30))
+
+
+
+#test trial
